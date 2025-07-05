b02351ce3c8039ac6c0b2f68185d8ee256ac3255
kartik-ashok <kartikkatti55@gmail.com>
2025-07-05 21:32:55 +0530

    Add Windows runner files for Flutter application
    - Created runner.exe.manifest to define application settings and DPI awareness.
    - Implemented utils.cpp and utils.h for console creation, command line argument handling, and UTF-16 to UTF-8 string conversion.
    - Developed win32_window.cpp and win32_window.h to manage a high DPI-aware Win32 window, including window creation, message handling, and theme updates.

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff42C83C);
+  static const lightBackGround = Color(0xffF2F2F2);
+  static const darkBackGround = Color(0xff0d0C0C);
+  static const darkGrey = Color(0xffBEBEBE);
+  static const grey = Color(0xff343434);
+}
+
+
---
+++ lib/core/config/theme/app_theme.dart
@@ -0,0 +1,18 @@
+import 'package:bloclearn/core/config/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackGround,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+}
